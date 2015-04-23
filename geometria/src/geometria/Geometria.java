4eaa84b262836a07c5b41ac28fb80113db023ea2
Elder XAvier <eldersergioxavier@gmail.com>
2015-04-23 09:17:56 -0300

    primeira Aula

---
+++ geometria/src/geometria/Geometria.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometria;
+
+import formularios.Menu;
+
+/**
+ *
+ * @author 1323594
+ */
+public class Geometria {
+
+    
+    public static void main(String[] args) {
+        Menu men = new Menu();
+        men.setVisible(true);
+        
+        //men.setIconImage(null);
+        
+    }
+    
+}
